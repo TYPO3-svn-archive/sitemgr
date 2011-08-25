085329ef8f5c43bec488271aaeca7435766d66f7
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-25 18:46:17 +0000

    [TASK] Refactoring
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sitemgr/trunk@51391 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ sitemgr_template/ext_tables.php
@@ -0,0 +1,5 @@
+<?php
+		//load template module if templavoila is active
+	Tx_Sitemgr_Utilities_CustomerModuleUtilities::registerModule('Tx_SitemgrTemplate_Modules_Template_TemplateController');
+
+?>
