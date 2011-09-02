38b26d31dd24b9e531e77318267fab048a6f856f
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-02 07:37:06 +0000

    [TASK] Refactoring
    added highly flexible Template Repositories.
    
    still Open Settingsscreen for each Template ...
    
    Related: #14072
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sitemgr/trunk@51630 735d13b6-9817-0410-8766-e36946ffe9aa

--- sitemgr_template/Resources/Public/JavaScripts/Modules/Template/main.js
+++ sitemgr_template/Resources/Public/JavaScripts/Modules/Template/main.js
@@ -1,253 +1,177 @@
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Kay Strobach (typo3@kay-strobach.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/*******************************************************************************
+ * Register Namespace
+ * Initialize some vars 
+ ******************************************************************************/ 	
+	Ext.ns('TYPO3.Sitemgr.TemplateApp');
+
+/*******************************************************************************
+ * Application object
+ ******************************************************************************/
 	Ext.onReady(function (){
-		var tpl = new Ext.XTemplate(
-				'<tpl for=".">',
-		            '<div class="item-wrap" id="structure{uid}" style="width:180px;height:180px;float:left;padding:2x;">',
-		            	'<small style="text-align:right;">{datastructure}</small>',
-						'<div class="thumb" style="text-align:center;padding:10px;height:100px;">{previewicon}</div>',
-						'<h3 style="text-align:center">{title}</h3>',
-				    	//'<!--<span>{description}</span>-->',
-				    	
+		TYPO3.Sitemgr.TemplateApp.init();
+	});
+	
+	TYPO3.Sitemgr.TemplateApp = {
+		tpl: new Ext.XTemplate(
+			'<tpl for=".">',
+				'<div class="template-item-wrap" id="structure{uid}">',
+						'<div class="thumb">',
+						'<img src="{icon}">',
+						'<small>{title}</small>',
 					'</div>',
-		        '</tpl>',
-		        '<div class="x-clear"></div>'
-			);
-		
-		Ext.getCmp('Sitemgr_App_Tabs').add({
-			title:'###LANG.title###',
-			layout:'hbox',
-			disabled:!TYPO3.settings.sitemgr.customerSelected,
-			layoutConfig: {
-				//padding:'5',
-				border:false,
-				align:'stretch'
-			},
-			defaults: {
-				flex:1
-			},
-			items:[
-				{
-					title:'###LANG.structure.title###',
-					autoScroll:true,
-					tbar:[
-						{
-							text:'###LANG.action.getDescriptionAndHints###',
-							iconCls:'typo3-csh-icon',
-							handler:function() {
-								if(Ext.getCmp('templateStructure').getSelectedRecords().length>0) {
-									desc = Ext.getCmp('templateStructure').getSelectedRecords()[0].data.description;
-									if(desc.substring(0,7)=='http://' || desc.substring(0,8)=='https://') {
-										window.open(desc);
-									} else {
-										win = new Ext.Window({
-											title:'###LANG.action.getDescriptionAndHints###',
-											closeAction:'destroy',
-											layout:'fit',
-							                width:500,
-							                height:300,
-							                modal:true,
-											items:[
-												{
-													html:desc
+				'</div>',
+				'</tpl>',
+			'<div class="x-clear"></div>'
+		),
+		init: function() {
+			this.tab = Ext.getCmp('Sitemgr_App_Tabs').add({
+				title:TYPO3.lang.SitemgrTemplates_title,
+				layout:'vbox',
+				disabled:!TYPO3.settings.sitemgr.customerSelected,
+				layoutConfig: {
+					//padding:'5',
+					border:false,
+					align:'stretch'
+				},
+				defaults: {
+					flex:1
+				},
+				items:[
+					{
+						autoScroll:true,
+						items: [
+							{
+								xtype:'dataview',
+								loadingText: TYPO3.lang.SitemgrTemplates_loading,
+								emptyText: TYPO3.lang.SitemgrTemplates_norecords,
+								id:'templateSelector',
+								selectedClass:'template-item-selected',
+								itemSelector:'div.template-item-wrap',
+								store:new Ext.data.DirectStore({
+									storeId:'templateStructureStore',
+									autoLoad:true,
+									directFn:TYPO3.sitemgr.tabs.dispatch,
+									paramsAsHash: false,
+									paramOrder:'module,fn,args',
+									baseParams:{
+										module:'sitemgr_template',
+										fn    :'getTemplates',
+										args  :''
+									},
+									idProperty: 'uid',
+									fields: [
+										'id',
+										'icon',
+										'screens',
+										'description',
+										'title',
+										'copyright',
+										'version'
+									],
+								}),
+								tpl: this.tpl,
+								singleSelect:true,
+								listeners: {
+									selectionchange: {
+										scope:this,
+										fn: function(dv,nodes){
+											record =  dv.getSelectedRecords()[0].data;
+											Ext.Msg.wait(
+												'<h3>' + record.title + '</h3>'
+												+ '<center><img src="' + record.icon + '"></center>'
+												+ '<p>' + record.description + '</p>'
+												,
+												TYPO3.lang.SitemgrTemplates_loadingForm
+											);
+											TYPO3.sitemgr.tabs.dispatch(
+												'sitemgr_template',
+												'getTemplateOptions',
+												record,
+												function(provider,response) {
+													Ext.Msg.hide();
+													win = new Ext.Window({
+														title:'###LANG.templateOptions.title###',
+														id   :'templateWindow',
+														modal:true,
+														closeAction: 'close',
+														border:false,
+														items:[
+															{
+																xtype:'form',
+																id:'templateForm',
+																autoHeight: true,
+																api:{
+																	submit:TYPO3.sitemgr.tabs.handleForm
+																},
+																border:false,
+																paramOrder: 'module,fn,args',
+																viewConfig : {
+														    		forceFit: true,
+																},
+																fileUpload:true,
+																labelWidth:260,
+																width:520,
+																defaults:{
+																	style:'margin:5px;',
+																	width:510
+																},items:[
+																	{
+																		xtype:'displayfield',
+																		html:'###LANG.field.templateHint###',
+																		hideLabel:true,
+																		cls:'typo3-message message-information'
+																	},{
+																		title:'Titel',
+																		xtype:'fieldset',
+																		defaultType:'textfield',
+																		defaults: {
+																			xtype:'textfield',
+																			width:200,
+																			msgTarget: 'side',
+																			style:'margin:5px;'
+																		},
+																		items:response.result.form
+																	}
+																]
+															}
+														]
+													});
+													win.show();
 												}
-											]
-										});
-										win.show();
+											);
+										}
 									}
 								}
 							}
-						}
-					],
-					items: [
-						{
-							xtype:'dataview',
-							loadingText:'###LANG.updateData###',
-							id:'templateStructure',
-							selectedClass:'selected',
-							itemSelector:'div.item-wrap',
-							store:new Ext.data.DirectStore({
-								storeId:'templateStructureStore',
-								autoLoad:true,
-								directFn:TYPO3.sitemgr.tabs.dispatch,
-								paramsAsHash: false,
-								paramOrder:'module,fn,args',
-								baseParams:{
-									module:'sitemgr_template',
-									fn    :'getStructures',
-									args  :''
-								},
-								idProperty: 'uid',
-								fields: [{
-							        name: 'uid',
-							        type: 'int'
-							    },
-							        'title',
-							        'previewicon',
-							        'description',
-							        'datastructure'
-							    ],
-							}),
-							tpl: tpl,
-							singleSelect:true,
-							emptyText: '###LANG.emptytext###',
-							listeners: {
-				            	selectionchange: {
-				            		scope:this,
-									fn: function(dv,nodes){
-				            			record =  dv.getSelectedRecords()[0].data.uid;
-				            			Ext.getCmp('templateColorTheme').enable();
-										Ext.getCmp('templateColorTheme').get(0).getStore().load({
-											params:{
-												args:record
-											}
-										});
-				            		}
-				            	}
-				            }
-						}
-					]
-				},{
-					title:'###LANG.colorTheme.title###',
-					id:'templateColorTheme',
-					disabled:true,
-					layoutConfig: {
-						padding:'0',
-						align:'stretch'
-					},
-					defaults: {
-						flex:1,
-						border:false
-					},
-					tbar:new Ext.Toolbar([
-						{
-							text:'&nbsp;',
-							disabled:true
-						}
-					]),
-					autoScroll:true,
-					items: [
-						{
-							xtype:'dataview',
-							loadingText:'###LANG.colorTheme.title###',
-							selectedClass:'selected',
-							itemSelector:'div.item-wrap',
-							store:new Ext.data.DirectStore({
-								storeId:'templateColorThemeStore',
-								autoLoad:false,
-								directFn:TYPO3.sitemgr.tabs.dispatch,
-								paramsAsHash: false,
-								paramOrder:'module,fn,args',
-								baseParams:{
-									module:'sitemgr_template',
-									fn    :'getColorThemes',
-									args  :''
-								},
-								idProperty: 'uid',
-								fields: [
-							    	'uid',
-								    'title',
-							        'previewicon',
-							        'description',
-							        'datastructure'
-							    ],
-							}),
-							tpl: tpl,
-							singleSelect:true,
-							emptyText: '###LANG.emptytext###',
-							listeners: {
-				            	selectionchange: {
-				            		scope:this,
-									fn: function(dv,nodes){
-										record =  dv.getSelectedRecords()[0].data.uid;
-										loadingMask.show();
-										TYPO3.sitemgr.tabs.dispatch(
-											'sitemgr_template',
-											'getTemplateOptions',
-											record,
-											function(provider,response) {
-												loadingMask.hide();
-												win = new Ext.Window({
-													title:'###LANG.templateOptions.title###',
-													id   :'templateWindow',
-													modal:true,
-													border:false,
-													items:[
-														{
-															xtype:'form',
-															id:'templateForm',
-															autoHeight: true,
-															api:{
-																submit:TYPO3.sitemgr.tabs.handleForm
-															},
-															border:false,
-															paramOrder: 'module,fn,args',
-															viewConfig : {
-													    		forceFit: true,
-															},
-															fileUpload:true,
-															labelWidth:260,
-															width:520,
-															defaults:{
-																style:'margin:5px;',
-																width:510
-															},
-															items:[
-																{
-																	xtype:'displayfield',
-																	html:'###LANG.field.templateHint###',
-																	hideLabel:true,
-																	cls:'typo3-message message-information'
-																},{
-																	title:'Titel',
-																	xtype:'fieldset',
-																	defaultType:'textfield',
-																	defaults: {
-																		xtype:'textfield',
-																		width:200,
-																		msgTarget: 'side',
-																		style:'margin:5px;'
-																	},
-																	items:response.result.form
-																}
-															]
-															
-														}
-													],
-													buttons:[
-														{
-															text:'###LANG.action.saveTemplate###',
-															iconCls:'t3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-save-close',
-															handler:function() {
-																Ext.getCmp('templateForm').getForm().submit(
-																{
-																	waitMsg: '###LANG.action.saveTemplate###',
-																	params: {
-																		module:'sitemgr_template',
-																		fn    :'saveTemplateSettings',
-																		args  :[
-																			record+':'+ksSitemgrTools.customerId
-																		]
-																	},
-																	success: function(f,a){
-																		Ext.getCmp('templateWindow').hide();
-																		Ext.getCmp('templateWindow').destroy();
-																	}
-																});
-															}
-														}
-													],
-													listeners:{
-														
-													}
-												});
-												win.show();
-											}
-										);
-				            		}
-				            	}
-				            }
-						}
-					]
-				}
-			]
-		});
-	});
+						]
+					}
+				]
+			});
+		}
+	};
--- sitemgr_template/ext_tables.php
+++ sitemgr_template/ext_tables.php
@@ -1,5 +1,8 @@
 <?php
 		//load template module if templavoila is active
-	Tx_Sitemgr_Utilities_CustomerModuleUtilities::registerModule('Tx_SitemgrTemplate_Modules_Template_TemplateController');
+	Tx_Sitemgr_Utilities_CustomerModuleUtilities::registerModule(
+		'sitemgr_template',
+		'Tx_SitemgrTemplate_Modules_Template_TemplateController'
+	);
 
 ?>
