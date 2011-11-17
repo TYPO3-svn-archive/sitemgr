058a7a8be28d2761c69d5dacb14f0d0af46fa73d
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-17 12:04:58 +0000

    [TASK] TER UPLOAD
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sitemgr/trunk@54362 735d13b6-9817-0410-8766-e36946ffe9aa

--- sitemgr_template/ext_emconf.php
+++ sitemgr_template/ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "ks_sitemgr".
+# Extension Manager/Repository config file for ext "sitemgr_template".
 #
-# Auto generated 01-02-2011 06:13
+# Auto generated 17-11-2011 13:01
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Kay Strobach',
 	'author_email' => 'typo3@kay-strobach.de',
 	'shy' => '',
-	'dependencies' => 'cms,extbase,fluid',
+	'dependencies' => 'sitemgr,cms,extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,24 +29,23 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'sitemgr'  => '2.0.0-2.0.99',
-			'cms'     => '',
+			'sitemgr' => '2.0.0-2.0.99',
+			'cms' => '',
 			'extbase' => '',
-			'fluid'   => '',
+			'fluid' => '',
 		),
 		'conflicts' => array(
-			
 		),
 		'suggests' => array(
-			'templavoila' => '1.4.0-1.5.99',			
+			'templavoila' => '1.4.0-1.5.99',
 		),
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:58:{s:13:"CHANGELOG.TXT";s:4:"b1f2";s:10:"README.txt";s:4:"ee2d";s:9:"Thumbs.db";s:4:"d6e4";s:21:"ext_conf_template.txt";s:4:"dd23";s:12:"ext_icon.gif";s:4:"4f5b";s:17:"ext_localconf.php";s:4:"8cd9";s:14:"ext_tables.php";s:4:"ae18";s:14:"ext_tables.sql";s:4:"f9b8";s:30:"icon_tx_sitemgr_customer.gif";s:4:"4f5b";s:16:"locallang_db.xml";s:4:"1154";s:7:"tca.php";s:4:"cf6f";s:14:"doc/manual.sxw";s:4:"33e9";s:19:"doc/wizard_form.dat";s:4:"6b36";s:20:"doc/wizard_form.html";s:4:"ecf1";s:36:"lib/class.tx_ks_sitemgr_customer.php";s:4:"3f86";s:34:"lib/class.tx_ks_sitemgr_direct.php";s:4:"413a";s:31:"lib/class.tx_ks_sitemgr_div.php";s:4:"1ab0";s:31:"lib/class.tx_ks_sitemgr_tab.php";s:4:"ed6d";s:39:"lib/class.tx_ks_sitemgr_toolbaritem.php";s:4:"3f02";s:39:"lib/class.tx_ks_sitemgr_userstylefe.php";s:4:"4f4f";s:13:"mod1/conf.php";s:4:"9c9e";s:18:"mod1/ext-icons.css";s:4:"6e9a";s:13:"mod1/extjs.js";s:4:"61c9";s:14:"mod1/index.php";s:4:"d71d";s:18:"mod1/locallang.xml";s:4:"7b5f";s:22:"mod1/locallang_mod.xml";s:4:"6855";s:22:"mod1/mod_template.html";s:4:"e74c";s:19:"mod1/moduleicon.gif";s:4:"dc56";s:31:"mod1/ext-icons/document-new.png";s:4:"b2a5";s:32:"mod1/ext-icons/document-open.png";s:4:"b986";s:38:"mod1/ext-icons/document-save-close.png";s:4:"20e7";s:32:"mod1/ext-icons/document-view.png";s:4:"a16f";s:27:"mod1/ext-icons/edit-add.png";s:4:"c577";s:30:"mod1/ext-icons/edit-delete.png";s:4:"33a3";s:40:"mod1/ext-icons/pagetree-backend-user.png";s:4:"6a47";s:46:"mod1/ext-icons/system-backend-user-emulate.png";s:4:"f7c6";s:35:"mod1/ext-icons/system-help-open.png";s:4:"c6fd";s:35:"mod1/ext-icons/system-list-open.png";s:4:"1817";s:28:"mod1/ext-icons/text-html.png";s:4:"5d86";s:46:"tabs/beuser/class.tx_ks_sitemgr_tab_beuser.php";s:4:"4fa6";s:20:"tabs/beuser/extjs.js";s:4:"ad4c";s:25:"tabs/beuser/locallang.xml";s:4:"de32";s:50:"tabs/customer/class.tx_ks_sitemgr_tab_customer.php";s:4:"637c";s:22:"tabs/customer/extjs.js";s:4:"50d8";s:27:"tabs/customer/locallang.xml";s:4:"7038";s:42:"tabs/help/class.tx_ks_sitemgr_tab_help.php";s:4:"71e6";s:18:"tabs/help/extjs.js";s:4:"99cc";s:23:"tabs/help/locallang.xml";s:4:"a161";s:66:"tabs/piwikintegration/class.tx_ks_sitemgr_tab_piwikintegration.php";s:4:"fa17";s:30:"tabs/piwikintegration/extjs.js";s:4:"4b15";s:35:"tabs/piwikintegration/locallang.xml";s:4:"2a37";s:50:"tabs/template/class.tx_ks_sitemgr_tab_template.php";s:4:"2b21";s:22:"tabs/template/extjs.js";s:4:"8e21";s:27:"tabs/template/locallang.xml";s:4:"b609";s:24:"tabs/template/screen.css";s:4:"eded";s:17:"toolbar/index.php";s:4:"dff9";s:22:"toolbar/ks_sitemgr.css";s:4:"b583";s:21:"toolbar/ks_sitemgr.js";s:4:"e0ae";}',
+	'_md5_values_when_last_written' => 'a:24:{s:16:"ext_autoload.php";s:4:"3540";s:12:"ext_icon.gif";s:4:"6759";s:14:"ext_tables.php";s:4:"fd60";s:46:"Classes/Domain/Model/TemplateAbstractModel.php";s:4:"750b";s:48:"Classes/Domain/Model/TemplateOldsitemgrModel.php";s:4:"ea55";s:58:"Classes/Domain/Model/TemplateTemplavoilaFrameworkModel.php";s:4:"b40b";s:49:"Classes/Domain/Model/TemplateTemplavoilaModel.php";s:4:"60d1";s:56:"Classes/Domain/Repository/TemplateAbstractRepository.php";s:4:"36b3";s:48:"Classes/Domain/Repository/TemplateRepository.php";s:4:"5c84";s:68:"Classes/Domain/Repository/TemplateTemplavoilaFrameworkRepository.php";s:4:"fbdf";s:59:"Classes/Domain/Repository/TemplateTemplavoilaRepository.php";s:4:"5e1e";s:47:"Classes/Modules/Template/TemplateController.php";s:4:"6b7a";s:50:"Resources/Private/Language/Constants/locallang.xml";s:4:"9375";s:57:"Resources/Private/Language/Modules/Template/locallang.xml";s:4:"c104";s:72:"Resources/Public/Contrib/ux-ColorField-ryanpetrelo/Ext.ux.ColorField.css";s:4:"bbd3";s:71:"Resources/Public/Contrib/ux-ColorField-ryanpetrelo/Ext.ux.ColorField.js";s:4:"8b3c";s:61:"Resources/Public/Contrib/ux-ColorField-ryanpetrelo/readme.txt";s:4:"90ad";s:70:"Resources/Public/Contrib/ux-ColorField-ryanpetrelo/images/gradient.png";s:4:"52fb";s:67:"Resources/Public/Contrib/ux-ColorField-ryanpetrelo/images/wheel.png";s:4:"415b";s:61:"Resources/Public/Contrib/ux-sitemgrTemplate/Ext.ux.patches.js";s:4:"1842";s:69:"Resources/Public/Contrib/ux-sitemgrTemplate/Ext.ux.sitemgrCombobox.js";s:4:"c175";s:71:"Resources/Public/Contrib/ux-sitemgrTemplate/Ext.ux.sitemgrMultiField.js";s:4:"8626";s:53:"Resources/Public/JavaScripts/Modules/Template/main.js";s:4:"e600";s:54:"Resources/Public/Stylesheets/Modules/Template/main.css";s:4:"abce";}',
 );
 
 ?>
