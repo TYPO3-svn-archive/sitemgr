dbb3cb1b7176dbe65ab5bddf187644f99cc8b045
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-01 13:40:09 +0000

    [TASK] Optimized ext_tables.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sitemgr/trunk@55210 735d13b6-9817-0410-8766-e36946ffe9aa

--- sitemgr_template/ext_tables.php
+++ sitemgr_template/ext_tables.php
@@ -1,9 +1,11 @@
 <?php
-		//load template module if templavoila is active
-	Tx_Sitemgr_Utilities_CustomerModuleUtilities::registerModule(
-		'sitemgr_template',
-		'Tx_SitemgrTemplate_Modules_Template_TemplateController',
-		'before:sitemgr_help'
-	);
+	if (TYPO3_MODE == 'BE') {
+			//load template module if templavoila is active
+		Tx_Sitemgr_Utilities_CustomerModuleUtilities::registerModule(
+			'sitemgr_template',
+			'Tx_SitemgrTemplate_Modules_Template_TemplateController',
+			'before:sitemgr_help'
+		);
+	}
 
 ?>
